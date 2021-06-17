e9554701a68a2729744a13a059bc3560c4decf30
LasithEranga <lasitheranga1@gmail.com>
2021-06-17 23:45:04 +0530

    Login UI

--- Fine Management System/Form1.cs
+++ Fine Management System/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Fine_Management_System
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
