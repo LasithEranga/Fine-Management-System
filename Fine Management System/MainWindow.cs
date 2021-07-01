417acf8d06e295cb747785971adbd457ee378fe0
LasithEranga <lasitheranga1@gmail.com>
2021-07-01 20:52:56 +0530

    UI Changes

--- Fine Management System/MainWindow.cs
+++ Fine Management System/MainWindow.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Fine_Management_System
@@ -15,6 +8,39 @@ namespace Fine_Management_System
         public MainWindow()
         {
             InitializeComponent();
+            home.SetBounds(0,0, 894, 552);
+            chart.Hide();
+        }
+
+        private void pictureBox10_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            home.Hide();
+            chart.SetBounds(0, 0, 894, 552);
+            chart.Show();
+            pictureBox5.Image = Properties.Resources.fineX;
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            chart.Hide();
+            home.Show();
+            pictureBox5.Image = Properties.Resources.homeBlue;
+        }
+
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            ControlExtension.Draggable(this, true);
+        }
+
+        private void drag(object sender, MouseEventArgs e)
+        {
+            
         }
     }
 }
